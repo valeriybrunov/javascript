3de85af337cdc817d3aa60977a66b49af6e7d6bb
Valerii Brunov <Valer1978@bk.ru>
2017-06-13 21:26:32 +0400

    13.06.2017

--- form_telefon.js
+++ form_telefon.js
@@ -1,133 +1,41 @@
 (function($) {
-
     $(document).ready(function() {
 
-    	/**
-    	 * v.0.3
-    	 * Объект tel упрощает ввод в текстовое поле номера сотового телефона, путём вставки шаблона-подсказки для ввода.
-    	 * 
-    	 * ПРИМЕР: Для текстового поля формы <input type="text" value="" name="tel" id="tel" maxlength="16">
-    	 * применять следующим образом:
-    	 * tel.id = 'tel';
-    	 * tel.init();
-    	 */
-        var tel = {
+        /**
+         * Упрощает ввод в текстовое поле номера сотового телефона, путём вставки шаблона-подсказки для ввода.
+         *
+         * 
+         */
 
-        	/**
-        	 * Свойсива.
-        	 */
-            // id поля ввода номера сотового телефона.
-            id: '',
-            // "Чистый" номер телефона, т.е. состоящий только из цифр. Например: 9379908790.
-            clean_tel: '',
+        var Form_telefon = function() {
+
+            // -------------------------------
+			// Приватные свойства и методы.
+			// -------------------------------
+
+            var id, input, start_pos, end_pos, form_value;
+
             // Шаблон-формат, в который будет вставлен номер сотового телефона.
-            template_tel: '+7(___)___-__-__',
-            // Счётчик кликов по форме ввода. Предназначен для вставки шаблона-формата при первом клике по форме.
-            count_click: 0,
-            // Счётчик возникновения события, когда позиция курсора становится равным 3.
-            count_pos_3: 0,
+            var template_tel = '+7(___)___-__-__';
+
+            // Начальная позиция в шаблоне для ввода телефонного номера.
+            function startPos() {
+                start_pos = template_tel.indexOf( '_' );
+            }
+
+            // Конечная позиция в шаблоне для ввода телефонного номера.
+            function endPos() {
+                end_pos = template_tel.length;
+            }
+
+            // Следующая позиция для ввода номера телефона.
+            // Работает непосредственно с полем ввода телефона.
+            function nextPos() {
+                var form_val = id.val();
+                return form_val.indexOf( '_' );
+            }
 
             /**
-             * Метод запуска.
-             */
-            init: function() {
-            	// Ищем поле ввода номера телефона.
-				var input = document.getElementById(this.id);
-				$('#' + this.id).on('click', function(eventObj) {
-					// Событие - клик по форме.
-					if (tel.count_click == 0) {
-						// Вставляем в поле ввода мобильного телефона аттрибут value с шаблоном ввода мобильного телефона.
-            			$('#' + tel.id).val(tel.template_tel);
-            			tel.count_click++;
-            		}
-					tel.click(eventObj, input);
-				});
-                $('#' + this.id).on('keyup', function(eventObj) {
-                	// Событие - возврат клавиши из нажатого состояния.
-					tel.keyup(eventObj, input);
-				});
-				$('#' + this.id).on('focusout', function(eventObj) {
-					// Событие -потери фокуса на элементе формы.
-					var count_ = input.value.split('_').length - 1;
-					if (count_ == 10) {
-						// Элемент формы для ввода номера сотового телефона не заполнен.
-						input.value = '';
-						tel.count_click = 0;
-					}
-				});
-            },
-
-            /**
-             * Метод события клика по форме.
-             * Метод установливает курсор только между цифрами, в начало или конец цифрового ряда. Установить курсор в те места формы,
-             * где символы "_" метод не представляет возможным.
-             *
-             * @param eventObj
-             *  Объект события.
-             * @param input
-             *  Объект элемента поля ввода номера телефона.
-             */
-            click: function(eventObj, input) {
-				if (this.clean_tel.length == 0) {
-					// Если поле пустое (пользователь не ввел ни одной цифры).
-					// Устанавливаем курсор в начало перед "+7(".
-					this.setCursor(input, 3);
-				}
-				else {
-					// Поле формы заполнено частично или полностью.
-					// Определяем позицию установки курсора пользователем при щелчке по полю формы.
-					var posCursor = this.getCursorPosition(input);
-					// Определяем длину строки clean_tel с учётом символов "+7(, ), -" шаблона template_tel.
-					var length_clean_tel = this.clean_tel.length;
-					if (length_clean_tel >= 1 && length_clean_tel <= 3)  var length_clean_tel_template_tel = length_clean_tel + 3;
-					if (length_clean_tel >= 4 && length_clean_tel <= 6)  var length_clean_tel_template_tel = length_clean_tel + 4;
-					if (length_clean_tel >= 7 && length_clean_tel <= 8)  var length_clean_tel_template_tel = length_clean_tel + 5;
-					if (length_clean_tel >= 9 && length_clean_tel <= 10) var length_clean_tel_template_tel = length_clean_tel + 6;
-					if (posCursor > (length_clean_tel_template_tel - 1)) {
-						// Если пользователь пытался установить курсор дальше длины строки - возвращаем его к концу цифрового ряда.
-						this.setCursor(input, length_clean_tel_template_tel);
-					}
-					else {
-						// Позиция курсора не может быть меньше 3.
-						if (posCursor < 3 || posCursor == undefined) this.setCursor(input, 3);
-						else this.setCursor(input, posCursor);
-					}
-					if (posCursor > 3) {
-						// Если позиция курсора в поле формы больше 3, обнуляем счётчик this.count_pos_3.
-						this.count_pos_3 = 0;
-					}
-				}
-			},
-
-			/**
-			 * Метод устанавливает курсор в указанную позицию. Любая установка курсора в поле формы должна идти через этот метод,
-			 * так как метод передвигает курсор за символы ")" и "-".
-			 *
-			 * @param input
-			 *  Объект элемента поля ввода номера телефона.
-			 * @param posCursor
-			 *  Позиция курсора в поле формы ввода номера сотвого телефона.
-			 */
-			setCursor: function(input, posCursor) {
-				if (posCursor == 6) {
-					this.setCursorPosition(input, 7, 7);
-				}
-				else {
-					if (posCursor == 10) {
-						this.setCursorPosition(input, 11, 11);
-					}
-					else {
-						if (posCursor == 13) {
-							this.setCursorPosition(input, 14, 14);
-						}
-						else {
-							this.setCursorPosition(input, posCursor, posCursor);
-						}
-					}
-				}
-			},
-
-			/**
 			 * Метод устанавливает курсор в нужную позицию внутри формы.
 			 *
 			 * @param oInput
@@ -139,18 +47,18 @@
 			 *
 			 * При совпадении oStart и oEnd курсор устанавливается в указанную позицию.
 			 */
-			setCursorPosition: function(oInput, oStart, oEnd) {
-                if (oInput.setSelectionRange) {
-					oInput.setSelectionRange(oStart, oEnd);
+			function setCursorPosition( oInput, oStart, oEnd ) {
+                if ( oInput.setSelectionRange ) {
+					oInput.setSelectionRange( oStart, oEnd );
 				}
-				else if (oInput.createTextRange) {
+				else if ( oInput.createTextRange ) {
 					range = oInput.createTextRange();
-					range.collapse(true);
-					range.moveEnd('character', oEnd);
-					range.moveStart('character', oStart);
+					range.collapse( true );
+					range.moveEnd( 'character', oEnd );
+					range.moveStart( 'character', oStart );
 					range.select();
 				}
-			},
+			}
 
             /**
              * Метод получает позицию курсора внутри формы.
@@ -158,138 +66,138 @@
              * @param input
 			 *  Объект элемента поля ввода номера телефона.
              */
-            getCursorPosition: function(input) {
-                if (input.selectionStart) {
+            function getCursorPosition( input ) {
+                if ( input.selectionStart ) {
                     // Стандартные браузеры MazillaFireFox, Opera, Chrom.
                     return input.selectionStart;
                 }
-                else if (document.selection) {
+                else if ( document.selection ) {
                     // IE.
                     input.focus();
                     var sel = document.selection.createRange();
                     var selLen = document.selection.createRange().text.length;
-                    sel.moveStart('character', -input.value.length);
+                    sel.moveStart( 'character', -input.value.length );
                     return sel.text.length - selLen;
                 }
-            },
+            }
 
-            /**
-             * Метод события возврата клавиши из нажатого состояния.
-             *
-             * @param eventObj
-             *  Объект события.
-             * @param input
-			 *  Объект элемента поля ввода номера телефона.
-             */
-            keyup: function(eventObj, input) {
-				// Определяем позицию установки курсора пользователем.
-				var posCursor = this.getCursorPosition(input);
-				// Так как в шаблоне template_tel позиция курсора будет отличаться от позиции курсора в clean_tel из-за
-				// символов: +7(, ), -, найдем позицию курсора в clean_tel.
-				if (posCursor >= 3 && posCursor <= 5) var posClean_tel = posCursor - 3;
-				if (posCursor >= 7 && posCursor <= 9) var posClean_tel = posCursor - 4;
-				if (posCursor >= 11 && posCursor <= 12) var posClean_tel = posCursor - 5;
-				if (posCursor >= 14 && posCursor <= 15) var posClean_tel = posCursor - 6;
-				// Определяем категорию кода.
-				if (eventObj.which >= 48 && eventObj.which <= 57) {
-					// Если передан цифровой код.
-					// По коду символа определяем его строковой символ.
-                    if (eventObj.which == 48) var symbol = '0';
-                    if (eventObj.which == 49) var symbol = '1';
-                    if (eventObj.which == 50) var symbol = '2';
-                    if (eventObj.which == 51) var symbol = '3';
-                    if (eventObj.which == 52) var symbol = '4';
-                    if (eventObj.which == 53) var symbol = '5';
-                    if (eventObj.which == 54) var symbol = '6';
-                    if (eventObj.which == 55) var symbol = '7';
-                    if (eventObj.which == 56) var symbol = '8';
-                    if (eventObj.which == 57) var symbol = '9';
-					// Вставляем символ в clean_tel, введёный с клавиатуры.
-					if (posClean_tel == 0) {
-						var startText = symbol;
-						var endText = this.clean_tel.substring(0, 9);
-						this.clean_tel = startText.concat(endText);
-					}
-					if (posClean_tel == 9) {
-						var startText = this.clean_tel.substring(0, 9);
-						var endText = symbol;
-						this.clean_tel = startText.concat(endText);
-					}
-					if (posClean_tel >= 1 && posClean_tel <= 8) {
-						var startText = this.clean_tel.substring(0, posClean_tel);
-						var endText = this.clean_tel.substring(posClean_tel, 9);
-						this.clean_tel = startText.concat(symbol, endText);
-					}
-					// Обрезаем лишние символы.
-					this.clean_tel = this.clean_tel.substr(0, 10);
-					this.insertInTemplate(input);
-					this.setCursor(input, posCursor + 1);
-				}
-				else if (eventObj.which == 8) {
-					// Если передан код Del.
-					if (posClean_tel == undefined) {
-						// Если переменная posClean_tel не определена. Это может быть в том случае, если позиция курсора попала после
-						// символа "-" или ")".
-						if (posCursor == 13) {
-							var posClean_tel = 7;
-							posCursor = 12;
-						}
-						if (posCursor == 10) {
-							var posClean_tel = 5;
-							posCursor = 9;
-						}
-						if (posCursor == 6) {
-							var posClean_tel = 2;
-							posCursor = 5;
-						}
-						if (posCursor <= 3) {
-							var posClean_tel = 0;
-							posCursor = 3;
-						}
-					}
-					var new_clean_tel = '';
-					if (posCursor == 3) {
-						// Если курсор достиг позиции 3.
-						this.count_pos_3++;
-					}
-					else this.count_pos_3 = 0;
-					if (this.count_pos_3 <= 1) {
-						for (var i = 0; i < 10; i++) {
-							if (i != posClean_tel) new_clean_tel = new_clean_tel + this.clean_tel.charAt(i);
-						}
-					}
-					else new_clean_tel = this.clean_tel;
-					this.clean_tel = new_clean_tel;
-					this.insertInTemplate(input);
-					this.setCursor(input, posCursor);
-				}
-			},
+            // Событие - клик по форме ввода.
+            function clickform() {
+                id.on('click', function( eventObj ) {
+                    var form_val = id.val();
+                    if ( form_val == '' ) {
+                        id.val( template_tel );
+                        setCursorPosition( input, start_pos, start_pos );
+                    }
+                    else if ( form_val == template_tel ) {
+                        setCursorPosition( input, start_pos, start_pos );
+                    }
+                    else if ( nextPos() == -1 ) {
+                        setCursorPosition( input, end_pos, end_pos );
+                    }
+                    else {
+                        var next_pos = nextPos();
+                        setCursorPosition( input, next_pos, next_pos );
+                    }
+                });
+            }
 
-			/**
-			 * Вставляет данные из this.clean_tel в шаблон this.template_tel, результат вставляет в форму.
-			 *
-			 * @param input
-			 *  Объект элемента поля ввода номера телефона.
-			 */
-			insertInTemplate: function(input) {
-                var new_template_tel = this.template_tel;
-                for (var i = 0; i < 10; i++) {
-                	if (this.clean_tel.charAt(i)) {
-                		// Если в this.clean_tel существует цифра.
-                		new_template_tel = new_template_tel.replace('_', this.clean_tel.charAt(i));
-                	}
-                	else break;
-                }
-                input.value = new_template_tel;
-			}
+            // Событие - потеря формой фокуса.
+            function focusout() {
+                id.on('focusout', function( eventObj ) {
+                    var form_val = id.val();
+                    if ( form_val == template_tel ) {
+                        id.val('');
+                    }
+                });
+            }
 
+            // Событие - перед нажатием клавиши (клавиша еще не отображена).
+            function keypress() {
+                id.on('keypress', function( eventObj ) {
+                    form_value = id.val();
+                });
+            }
+
+            // Событие - возвращение клавиши в ненажатое состояние.
+            function keyup() {
+                id.on('keyup', function( eventObj ) {
+                    id.val( form_value );
+                    var next_pos = nextPos();
+                    if ( eventObj.which >= 48 && eventObj.which <= 57 ) {
+                        if ( next_pos != -1 ) {
+                            if ( eventObj.which == 48 ) var symbol = '0';
+                            if ( eventObj.which == 49 ) var symbol = '1';
+                            if ( eventObj.which == 50 ) var symbol = '2';
+                            if ( eventObj.which == 51 ) var symbol = '3';
+                            if ( eventObj.which == 52 ) var symbol = '4';
+                            if ( eventObj.which == 53 ) var symbol = '5';
+                            if ( eventObj.which == 54 ) var symbol = '6';
+                            if ( eventObj.which == 55 ) var symbol = '7';
+                            if ( eventObj.which == 56 ) var symbol = '8';
+                            if ( eventObj.which == 57 ) var symbol = '9';
+                            form_value = form_value.replace( '_', symbol );
+                            id.val( form_value );
+                            setCursorPosition( input, next_pos + 1, next_pos + 1 );
+                        }
+                        else setCursorPosition( input, end_pos, end_pos );
+                    }
+                    else if ( eventObj.which == 8 ) {
+						if ( next_pos == -1 || (next_pos - 1) >= start_pos ) {
+                            var result;
+                            for (var i = 0; i <= (end_pos - 1); i++) {
+                                if ( !isNaN(form_value.charAt(i)) ) result = i;
+                            }
+                            var new_str = '';
+                            for (var i = 0; i <= (end_pos - 1); i++) {
+                                if ( result == i ) new_str = new_str + '_';
+                                else new_str = new_str + form_value.charAt(i);
+                            }
+							id.val( new_str );
+							next_pos = nextPos();
+							setCursorPosition( input, next_pos, next_pos );
+						}
+                        else {
+                            setCursorPosition( input, start_pos, start_pos );
+                        }
+                    }
+                });
+            }
+
+            return {
+
+				// -------------------------------
+				// Публичные методы и свойства.
+				// -------------------------------
+
+                // Инициализация объекта.
+                init: function( id_form ) {
+                    startPos();
+                    endPos();
+                    clickform();
+                    focusout();
+                    keypress();
+                    keyup();
+                },
+
+                // Шаблон-подсказка.
+                template: function( template ) {
+                    template_tel = template;
+                },
+
+                // id поля для ввода номера телефона.
+                id: function( id_tel ) {
+                    id = $( '#' + id_tel );
+                    input = document.getElementById( id_tel );
+                },
+
+            };
         };
 
-        // ПРИМЕР: Для текстового поля с id = 'tel'.
-        // Инициализация объекта.
-        tel.id = 'tel';
-        tel.init();
+        var Telefon = new Form_telefon();
+        Telefon.id( 'tel' );
+        Telefon.template( '(___)___-__-__' );
+        Telefon.init();
 
     });
-
 })(jQuery);
